ef13a70610d814e56baa48dd25a4dfad295718bb
Anshum25 <152506991+Anshum25@users.noreply.github.com>
2025-08-18 12:35:31 +0530

    Update HomepageCarousel.tsx

--- client/components/HomepageCarousel.tsx
+++ client/components/HomepageCarousel.tsx
@@ -155,7 +155,7 @@ export default function HomepageCarousel({
   // Multiple images - show carousel
   return (
     <div 
-      className={`absolute inset-0 flex flex-col items-center justify-center w-full h-full md:relative md:h-auto ${className}`} 
+      className={`relative flex flex-col items-center justify-center w-full h-auto ${className}`} 
       ref={carouselRef}
       tabIndex={0}
       role="region"
